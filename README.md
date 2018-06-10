02cc119abf6b2ad66bed28eaeeb271b4617da159
Colm Hughes <cjhugo1991@hotmail.com>
2018-06-10 16:55:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Install the following.
+1. sudo pip3 install Flask
+2. sudo pip3 install Flask-RESTful
+3. sudo pip3 install Flask-JWT
+
+Python has sqlite3 built in so we don't have to install that.
+
+### test.py.
+
+Is an introduction to sqlite3, independent of the project.
+Shows how to connect to a database, add a table, add rows and query the database.
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
+import os
+
+
+app = Flask(__name__)
+app.secret_key = "colm"
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #JWT class creates a new endpoint /auth, when we call /auth we send it a username and password that is used in authenticate and identity.
+
+
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
+
+if __name__ == "__main__":  #This is so if we do a from app import in another file it doesn't run the app. It will only run if we run app.py. 
+                            #When python runs a file it assigns a name to it and that name is __main__                            
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
